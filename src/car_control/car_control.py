a4a39fe1524d29c6b81963ca75718a491d347b75
Linh Nguyen <nvl1109@gmail.com>
2015-03-20 22:28:21 +0700

    Can catch key press/release event.

--- src/car_control/car_control.py
+++ src/car_control/car_control.py
@@ -11,6 +11,49 @@ from python_qt_binding.QtGui import QKeySequence, QShortcut, QWidget
 from rqt_gui_py.plugin import Plugin
 
 
+class CarControlWidget(QWidget):
+    def __init__(self, parent):
+        super(CarControlWidget, self).__init__(parent=parent)
+
+        self.initUi()
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == Qt.Key_Left:
+            print('Left Arrow Pressed')
+        elif key == Qt.Key_Right:
+            print('Right Arrow Pressed')
+        elif key == Qt.Key_Up:
+            print('Up Arrow Pressed')
+        elif key == Qt.Key_Down:
+            print('Down Arrow Pressed')
+
+    def keyReleaseEvent(self, event):
+        key = event.key()
+        if key == Qt.Key_Left:
+            print('Left Arrow Released')
+        elif key == Qt.Key_Right:
+            print('Right Arrow Released')
+        elif key == Qt.Key_Up:
+            print('Up Arrow Released')
+        elif key == Qt.Key_Down:
+            print('Down Arrow Released')
+
+    def mouseMoveEvent(self, event):
+        pos = event.pos()
+
+        print "x: %d - y: %d" % (event.x(), event.y())
+
+    def initUi(self):
+        self.setFixedHeight(200)
+        self.setFixedWidth(200)
+        # self.setMouseTracking(True)
+        self.grabKeyboard()
+
+    def shutdown(self):
+        self.releaseKeyboard()
+
+
 class CarControl(Plugin):
 
 
@@ -20,18 +63,13 @@ class CarControl(Plugin):
 
         self._publisher = None
 
-        self._widget = QWidget()
+        self._widget = CarControlWidget(None)
         if context.serial_number() > 1:
             self._widget.setWindowTitle("Car Control" + (' (%d)' % context.serial_number()))
         context.add_widget(self._widget)
 
-
-    @Slot(str)
-    def _on_topic_changed(self, topic):
-        pass
-
     def shutdown_plugin(self):
-        pass
+        self._widget.shutdown()
 
     def save_settings(self, plugin_settings, instance_settings):
         pass
