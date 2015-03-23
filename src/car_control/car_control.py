7e8cb902a7721693abe1579393705f7c9bc3d19b
Linh Nguyen <nvl1109@gmail.com>
2015-03-23 21:10:32 +0700

    Update

--- src/car_control/car_control.py
+++ src/car_control/car_control.py
@@ -4,6 +4,8 @@
 from __future__ import division
 import os
 import rospkg
+import threading
+import time
 
 import rospy
 from python_qt_binding.QtCore import Qt, QTimer, Slot
@@ -17,29 +19,49 @@ class CarControlWidget(QWidget):
     def __init__(self, parent):
         super(CarControlWidget, self).__init__(parent=parent)
 
+        self._leftDir = 0
+        self._rightDir = 0
+
+        self._pushThread = None
+        self._isStop = False
+
+        self._upKey = False
+        self._downKey = False
+        self._leftKey = False
+        self._rightKey = False
+
         self.initUi()
+        self.initCar()
 
     def keyPressEvent(self, event):
         key = event.key()
         if key == Qt.Key_Left:
             rospy.logdebug('Left Arrow Pressed')
+            self._leftKey = True
         elif key == Qt.Key_Right:
             rospy.logdebug('Right Arrow Pressed')
+            self._rightKey = True
         elif key == Qt.Key_Up:
             rospy.logdebug('Up Arrow Pressed')
+            self._upKey = True
         elif key == Qt.Key_Down:
             rospy.logdebug('Down Arrow Pressed')
+            self._downKey = True
 
     def keyReleaseEvent(self, event):
         key = event.key()
         if key == Qt.Key_Left:
             rospy.logdebug('Left Arrow Released')
+            self._leftKey = False
         elif key == Qt.Key_Right:
             rospy.logdebug('Right Arrow Released')
+            self._rightKey = False
         elif key == Qt.Key_Up:
             rospy.logdebug('Up Arrow Released')
+            self._upKey = False
         elif key == Qt.Key_Down:
             rospy.logdebug('Down Arrow Released')
+            self._downKey = False
 
     def mouseMoveEvent(self, event):
         pos = event.pos()
@@ -47,15 +69,87 @@ class CarControlWidget(QWidget):
         rospy.logdebug("x: %d - y: %d" % (event.x(), event.y()))
 
     def initUi(self):
-        self.setFixedHeight(600)
-        self.setFixedWidth(600)
+        self.setFixedHeight(100)
+        self.setFixedWidth(100)
         # self.setMouseTracking(True)
         self.grabKeyboard()
         rospy.loginfo("CarControl: keyboard grabed. Start control your car.")
 
+    def initCar(self):
+        self._publisher = rospy.Publisher('pi_motor_control', Motor, queue_size=10)
+        # rospy.init_node('pi_car', anonymous=True)
+        # Create push message thread
+        self._pushThread = threading.Thread(target=self._pushMsgExec)
+        self._pushThread.start()
+
+    def processKeys(self):
+        # Process the key status then update motor direction
+        if (self._upKey and self._downKey):
+            self._leftDir = 0
+            self._rightDir = 0
+            return
+        if (self._leftKey and self._rightKey):
+            self._leftDir = 0
+            self._rightDir = 0
+            return
+        if self._upKey:
+            if self._leftKey:
+                self._leftDir = 0
+                self._rightDir = 100
+            elif self._rightKey:
+                self._leftDir = 100
+                self._rightDir = 0
+            else:
+                self._leftDir = 100
+                self._rightDir = 100
+        elif self._downKey:
+            if self._leftKey:
+                self._leftDir = 0
+                self._rightDir = -100
+            elif self._rightKey:
+                self._leftDir = -100
+                self._rightDir = 0
+            else:
+                self._leftDir = -100
+                self._rightDir = -100
+        else:
+            if self._leftKey:
+                self._leftDir = -100
+                self._rightDir = 100
+            elif self._rightKey:
+                self._leftDir = 100
+                self._rightDir = -100
+            else:
+                self._leftDir = 0
+                self._rightDir = 0
+
+
+    def _pushMsgExec(self):
+        prevLeft = 0
+        prevRight = 0
+        while not self._isStop:
+            self.processKeys()
+            if not (prevLeft == prevRight == self._leftDir == self._rightDir == 0):
+                # If has control signal
+                m = Motor()
+                m.left = self._leftDir
+                m.right = self._rightDir
+                if self._isStop:
+                    m.left = 0
+                    m.right = 0
+                self._publisher.publish(m)
+                prevLeft = self._leftDir
+                prevRight = self._rightDir
+                rospy.loginfo("CarControl: send message left:%d right:%d" % (self._leftDir, self._rightDir))
+            time.sleep(0.1)
+
     def shutdown(self):
         self.releaseKeyboard()
-        rospy.loginfo("CarControl: shutdown")
+        rospy.loginfo("CarControl: shutting down")
+        self._isStop = True
+        if self._pushThread:
+            self._pushThread.join()
+        rospy.loginfo("CarControl: shutdown DONE")
 
 
 class CarControl(Plugin):
